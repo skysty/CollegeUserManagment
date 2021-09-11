405ef58abb02396e816df25a5190558a113e3843
erbol <erbol@DESKTOP-V56K8EL>
2021-09-11 19:40:46 +0600

    Add project files.

---
+++ CollegeUserManagment/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CollegeUserManagment.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeUserManagment.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options )
+            :base(options)
+        {
+        }
+        public DbSet<Students> Students { get; set; }
+    }
+}
---
+++ CollegeUserManagment/Migrations/20210911122547_init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CollegeUserManagment.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Patronomyc = table.Column<string>(nullable: true),
+                    BirthYear = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
